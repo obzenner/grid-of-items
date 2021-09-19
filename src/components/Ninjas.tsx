34f7647346c965873a02aea17ad4472c838b6d8f
Dmitry Lambrianov <dmitry.lambrianov@nentgroup.com>
2021-09-19 19:08:58 +0200

    feature(sort) :: init sort + intersection observer

--- src/components/Ninjas.tsx
+++ src/components/Ninjas.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import styled from 'styled-components';
+import { sortBy } from 'lodash';
 
 import type { Ninjas, Ninja } from '../../utils/generate-grid-items';
-import TrackVisibility from 'react-on-screen';
+import { useInView } from 'react-intersection-observer';
 
 const NinjasWrapper = styled.div`
   display: flex;
@@ -64,34 +65,55 @@ const AvatarWrapper = styled.img<AvatarWrapperProps>`
 
 `;
 
-const NinjaBox = (props: { ninja: Ninja, isVisible?: boolean }) => {
+const NinjaBox = (props: { ninja: Ninja }) => {
   const [isAvatarVisible, showAvatar] = React.useState(false);
+  const { ref, inView } = useInView({ trackVisibility: true, delay: 300, triggerOnce: true });
+
   const { name, flagAndCity, avatar } = props.ninja;
-  return (<NinjaWrapper>
-      <NinjaContentWrapper> 
-        <h4>{props.isVisible && name}</h4>
-        <NinjaAvatar>
-          {props.isVisible && <AvatarWrapper isAvatarVisible={isAvatarVisible} onLoad={() => {
-            showAvatar(true)
-          }} src={avatar} />}
-        </NinjaAvatar>
-        <h5>{props.isVisible && flagAndCity}</h5>
-      </NinjaContentWrapper>
+
+  return (<NinjaWrapper ref={ref}>
+    <NinjaContentWrapper>
+      <NinjaAvatar>
+        {inView && <AvatarWrapper isAvatarVisible={isAvatarVisible} onLoad={() => {
+          showAvatar(inView)
+        }} src={avatar} />}
+      </NinjaAvatar>
+      <h4>{inView && name}</h4>
+      <h5>{inView && flagAndCity}</h5>
+    </NinjaContentWrapper>
   </NinjaWrapper>)
 }
 
+const sortByParam= (data: Ninja[] | null, sortParam: string) => {
+  return sortBy(data, sortParam);
+}
 
 // Ninja List
-const NinjasList = (props: { data?: Ninjas}) => {
+const NinjasList = (props: { data?: Ninjas }) => {
+  const [ninjas, setNinjas] = React.useState<Ninja[] | null>(props.data && props.data.ninjas || null);
+  const [isNameSorted, sortByName] = React.useState(true)
+  const [isLocationSorted, sortByLocation] = React.useState(false)
+
   if (!props.data) {
     return (<>{'No Ninjas Found'}</>);
   }
-  return (<NinjasWrapper>{props.data && props.data.ninjas.map(ninja => {
-    return (<TrackVisibility key={ninja.name} partialVisibility once offset={500}>
-      <NinjaBox ninja={ninja} />
-    </TrackVisibility>)
-  }
-  )}</NinjasWrapper>)
+  return (<div>
+    <button onClick={() => {
+      sortByName(!isNameSorted);
+      sortByLocation(isNameSorted);
+      setNinjas(sortByParam(ninjas, 'name'));
+    }}>Sort by name: {`${isNameSorted}`}</button>
+    <button onClick={() => {
+      sortByLocation(!isLocationSorted);
+      sortByName(isLocationSorted);
+      setNinjas(sortByParam(ninjas, 'flagAndCity'));
+    }}>Sort by location: {`${isLocationSorted}`}</button>
+    <NinjasWrapper>
+      {ninjas && ninjas.map(ninja => {
+        return (<NinjaBox key={ninja.name} ninja={ninja} />)
+      })}
+    </NinjasWrapper>
+  </div>)
 
 }
 
