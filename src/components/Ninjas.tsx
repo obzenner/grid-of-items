12cc40d388bcad7bb2c8bc86825334499b875748
Dmitry Lambrianov <dmitry.lambrianov@nentgroup.com>
2021-09-19 17:50:24 +0200

    feature(img) :: add loading opacity animation and types

--- src/components/Ninjas.tsx
+++ src/components/Ninjas.tsx
@@ -1,22 +1,91 @@
-// src/public/components/Hello.js
 import * as React from "react";
+import styled, { keyframes, css } from 'styled-components';
+
 import type { Ninjas, Ninja } from '../../utils/generate-grid-items';
+import TrackVisibility from 'react-on-screen';
 
-const NinjaBox = (props: { ninja: Ninja }) => {
-  const { name, flagAndCity, avatar } = props.ninja;
+const NinjasWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 800px;
+  margin: auto;
+`;
 
-  const [finalName, setName] = React.useState(name)
+const NinjaWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 185px;
+  margin: 5px;
+  justify-content: center;
+  border: 1px solid #efefef;
+`;
 
-  return (<>
-    <h1 onClick={() => setName('my life is so... weird')}>{finalName}</h1>
-  </>)
+
+const NinjaContentWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  h4, h5
+   {
+    width: 100%;
+    min-height: 25px;
+    text-align: center;
+  }
+`;
+
+const NinjaAvatar = styled.div`
+  display: flex;
+  width: 90%;
+  height: 200px;
+  overflow: hidden;
+
+  img {
+    height: 100%;
+  }
+`;
+
+
+type AvatarWrapperProps = {
+  isAvatarVisible: boolean
 }
 
+const AvatarWrapper = styled.img<AvatarWrapperProps>`
+  opacity: ${props => props.isAvatarVisible ? '1' : '0'};
+  transition: opacity 0.7s ease-in-out;
+
+`;
+
+const NinjaBox = (props: { ninja: Ninja, isVisible?: boolean }) => {
+  const [isAvatarVisible, showAvatar] = React.useState(false);
+  const { name, flagAndCity, avatar } = props.ninja;
+  return (<NinjaWrapper>
+      <NinjaContentWrapper> 
+        <h4>{props.isVisible && name}</h4>
+        <NinjaAvatar>
+          {props.isVisible && <AvatarWrapper isAvatarVisible={isAvatarVisible} onLoad={() => {
+            showAvatar(true)
+          }} src={avatar} />}
+        </NinjaAvatar>
+        <h5>{props.isVisible && flagAndCity}</h5>
+      </NinjaContentWrapper>
+  </NinjaWrapper>)
+}
+
+
+// Ninja List
 const NinjasList = (props: { data?: Ninjas}) => {
   if (!props.data) {
     return (<>{'No Ninjas Found'}</>);
   }
-  return (<>{props.data && props.data.ninjas.map(ninja => <NinjaBox ninja={ninja} />)}</>)
+  return (<NinjasWrapper>{props.data && props.data.ninjas.map(ninja => {
+    return (<TrackVisibility key={ninja.name} once offset={500}>
+      <NinjaBox ninja={ninja} />
+    </TrackVisibility>)
+  }
+  )}</NinjasWrapper>)
 
 }
 
